9f41f9c5ab1ddcf982bd5bbaa216efb131e26f8b
branckson123 <ddr15153759@gmail.com>
2025-04-26 11:55:08 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	//qual a primeira letra do meu nome//
+	public static void main(String[] args) {
+	    char primeira_letra_nome = 'D';
+            System.out.println(primeira_letra_nome);
+    //qual minha idade//
+	    int minha_idade = 15;
+		    System.out.println(minha_idade);
+		boolean tomei_cafe_hoje = true; 
+		    System.out.println(tomei_cafe_hoje);
+	} 
+}
