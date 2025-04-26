100b7904be94c9370845f44cd23df105c0b43dd0
branckson123 <ddr15153759@gmail.com>
2025-04-26 11:50:41 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+	double laranja = 15.20;
+	double banana = 32.99;
+	double soma = laranja + banana;
+	double desconto = soma *0.2;
+	
+	System.out.println(laranja);
+	System.out.println(banana);
+	System.out.println(soma);
+	System.out.println(desconto);
+	}
+}
